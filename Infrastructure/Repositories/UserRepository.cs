57f72879f751dde1f6ef8aadf0d3575ada14fc53
Hawaiikiki <45608699+Hawaiikiki@users.noreply.github.com>
2022-07-26 13:54:58 -0700

    Added Login Page

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.RepositoryContracts;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MovieShopDbContext _movieShopDbContext;
+
+        public UserRepository(MovieShopDbContext movieShopDbContext)
+        {
+            _movieShopDbContext = movieShopDbContext;
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            _movieShopDbContext.Users.Add(user);
+            await _movieShopDbContext.SaveChangesAsync(); // need to call savechangesAsync to actually save to database
+            return user;
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            // this method will be used in accountService to check if there's matching user
+            var user = await _movieShopDbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+            return user;
+        }
+    }
+}
--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceContracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAccountService _accountService;
         [HttpGet]
         public IActionResult Register()
         {
@@ -16,7 +18,24 @@ namespace MovieShopMVC.Controllers
         public async Task<IActionResult> Register(UserRegisterModel model)
         {
             // when user registers account
+            // it'll use service and hash the password to save in database
+            var user = await _accountService.CreateUser(model);
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginModel model)
+        {
+            var user = await _accountService.ValidateUser(model);
+            if (user == false)
+            {
+                return View(model);
+            }
+            return LocalRedirect("~/");
         }
     }
 }

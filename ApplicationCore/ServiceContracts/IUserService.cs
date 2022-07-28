49c99dcf53f673f7206863ceca2f0e5862782257
Hawaiikiki <45608699+Hawaiikiki@users.noreply.github.com>
2022-07-27 17:55:00 -0700

    Created Purchase Entity and updated DbContext

---
+++ ApplicationCore/ServiceContracts/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.ServiceContracts
+{
+    public interface IUserService
+    {
+        Task PurchaseMovie(PurchaseRequestModel purchaseRequest, int userId);
+        Task IsMoviePurchased(PurchaseRequestModel purchaseRequest, int userId);
+        Task GetAllPurchasesForUser(int id);
+        Task GetPurchasesDetails(int userId, int movieId);
+        Task AddFavorite(FavoriteRequestModel favoriteRequest);
+        Task RemoveFavorite(FavoriteRequestModel favoriteRequest);
+        Task FavoriteExists(int id, int movieId);
+        Task GetAllFavoritesForUser(int id);
+        Task AddMovieReview(ReviewRequestModel reviewRequest);
+        Task UpdateMovieReview(ReviewRequestModel reviewRequest);
+        Task DeleteMovieReview(int userId, int movieId);
+        Task GetAllReviewsByUser(int id);
+    }
+}
